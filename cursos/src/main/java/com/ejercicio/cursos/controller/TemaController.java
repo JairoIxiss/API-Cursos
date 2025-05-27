b4b546a39e4d041bf4d9572e4407cbd1baf5baf2
JairoIxiss <jairoixis7@gmail.com>
2025-05-27 14:45:48 -0600

    Agregado a Controller de Tema

--- cursos/src/main/java/com/ejercicio/cursos/controller/TemaController.java
+++ cursos/src/main/java/com/ejercicio/cursos/controller/TemaController.java
@@ -3,23 +3,39 @@ package com.ejercicio.cursos.controller;
 import com.ejercicio.cursos.model.Tema;
 import com.ejercicio.cursos.service.TemaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
+@RequestMapping ("/temas")
 public class TemaController {
     @Autowired
     private TemaService temServ;
     
-    @PostMapping("/temas/crear")
+    @PostMapping("/crear")
     public String crearTema(@RequestBody Tema tem){
         temServ.saveTema(tem);
         return "El tema se ha creado con exito";
     }
-    
-    @PutMapping("/temas/editar")
+
+    @GetMapping("/traer")
+    public List<Tema> traerTemas(){
+        return temServ.getTemas();
+    }
+
+    @GetMapping("/traer/{id}")
+    public Tema traerTema(@PathVariable Long id){
+        return temServ.findTema(id);
+    }
+
+    @DeleteMapping("/eliminar/{id}")
+    public String eliminarTema(@PathVariable Long id){
+        temServ.deleteTema(id);
+        return "El tema se ha eliminado de forma correcta";
+    }
+
+    @PutMapping("/editar")
     public Tema editarTema(@RequestBody Tema tem){
         temServ.editTema(tem);
         return temServ.findTema(tem.getId_tema());
